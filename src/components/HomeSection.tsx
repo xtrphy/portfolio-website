ec31aa4923094c6db420eff9477e64a3d478f7c3
xtrphy <perepeckinvlad@gmail.com>
2025-07-31 00:00:21 +0600

    styles: changes in layout in  the Home Section

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -20,22 +20,37 @@ const HomeSection = () => {
                 className='relative h-screen w-full flex items-center overflow-hidden'
                 ref={ref}
             >
-                <div className='ml-[120px] flex flex-col gap-5 text-7xl'>
-                    <h1 className='relative italic font-black'>
-                        владислав перепечкин.
+                <div className='ml-[120px] flex flex-col gap-10 text-7xl'>
+                    <h1 className='italic font-black'>
+                        Владислав Перепечкин
                         <motion.div
                             style={{ y }}
                         >
-                            <Badge className='absolute bottom-7 -right-25 -rotate-2 text-lg bg-white/10 backdrop-blur-md text-[#444] border border-white/30 shadow-md px-4 py-1 rounded-xl ring-1 ring-white/20 hover:-rotate-3 hover:scale-103 hover:ring-black transition-all duration-200'>
+                            <Badge className='absolute -top-13 -right-25 -rotate-2 text-base bg-white/10 backdrop-blur-sm text-[#444] border border-white/30 shadow-md px-4 py-1 rounded-xl ring-1 ring-white/20 hover:-rotate-4 hover:scale-103 hover:ring-black transition-all duration-200'>
                                 фронтенд разработчик
+                            </Badge>
+                            <Badge className='absolute -top-3 -right-25 -rotate-2 text-base bg-white/10 backdrop-blur-sm text-[#444] border border-white/30 shadow-md px-4 py-1 rounded-xl ring-1 ring-white/20 hover:-rotate-4 hover:scale-103 hover:ring-black transition-all duration-200'>
+                                внимание к деталям
+                            </Badge>
+                            <Badge className='absolute top-7 -right-25 -rotate-2 text-base bg-white/10 backdrop-blur-sm text-[#444] border border-white/30 shadow-md px-4 py-1 rounded-xl ring-1 ring-white/20 hover:-rotate-4 hover:scale-103 hover:ring-black transition-all duration-200'>
+                                командная работа
+                            </Badge>
+                            <Badge className='absolute top-17 -right-25 -rotate-2 text-base bg-white/10 backdrop-blur-sm text-[#444] border border-white/30 shadow-md px-4 py-1 rounded-xl ring-1 ring-white/20 hover:-rotate-4 hover:scale-103 hover:ring-black transition-all duration-200'>
+                                ответственность
                             </Badge>
                         </motion.div>
                     </h1>
 
                     <ul className='flex flex-col gap-2 text-[#444] text-2xl tracking-wide list-none'>
-                        <li className='flex items-center gap-3'><CircleCheck color='green' strokeWidth={3} />креативный и внимательный к деталям.</li>
-                        <li className='flex items-center gap-3'><CircleCheck color='green' strokeWidth={3} />делаю так, что хочется возвращаться</li>
-                        <li className='flex items-center gap-3'><CircleCheck color='green' strokeWidth={3} />цель номер один: уникальность и функционал</li>
+                        <li className='flex items-center gap-3'>
+                            <CircleCheck color='green' strokeWidth={3} />креативный и внимательный к деталям.
+                        </li>
+                        <li className='flex items-center gap-3'>
+                            <CircleCheck color='green' strokeWidth={3} />делаю так, что хочется возвращаться
+                        </li>
+                        <li className='flex items-center gap-3'>
+                            <CircleCheck color='green' strokeWidth={3} />цель номер один: уникальность и функционал
+                        </li>
                     </ul>
 
                     <div className='flex gap-3 mt-5'>
@@ -52,7 +67,7 @@ const HomeSection = () => {
                 <motion.div
                     className='absolute -right-10 bottom-0 z-5 opacity-70 drop-shadow-[10px_5px_15px_rgba(0,0,0,0.15)]'
                     style={{ y }}
-                    animate={{ rotate: [0.9, -0.9, 0.9] }}
+                    animate={{ rotate: [0.7, -0.7, 0.7] }}
                     transition={{
                         duration: 5,
                         repeat: Infinity,
@@ -63,8 +78,8 @@ const HomeSection = () => {
                     <Image
                         src='/glass_woman1.png'
                         alt='Glass woman'
-                        width={850}
-                        height={850}
+                        width={900}
+                        height={900}
                         className='pointer-events-none select-none'
                     />
                 </motion.div>
