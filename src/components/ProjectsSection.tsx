f129de765025ac92eeebd20a1942675c57f07209
xtrphy <perepeckinvlad@gmail.com>
2025-08-02 15:48:55 +0600

    feat: create SocialMedia component, add 2 more projects for ProjectsSection

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
-import ProjectCard from './ProjectCard';
+import ProjectCard from './custom/ProjectCard';
 
 const ProjectsSection = () => {
     return (
-        <section className='flex justify-center' id='projects'>
+        <section className='relative flex justify-center overflow-clip' id='projects'>
 
-            <div className='pt-32 pb-32 flex flex-col justify-center items-center gap-5 max-w-6xl'>
+            <div className='pb-16 pt-32 flex flex-col justify-center items-center gap-5'>
                 <h2 className='text-6xl font-black mb-10'>Проекты</h2>
 
-                <div className='flex flex-wrap gap-6'>
+                <div className='grid gap-6 grid-cols-3'>
                     <ProjectCard
                         imageUrl='/pddapp-laptop.png'
                         title='"Кулагер" ПДД Тесты'
                         description='Приложение для решения ПДД тестов, подготовки к реальному тестированию. Таблицы и графики позволят отслеживать ваш прогресс.'
-                        badges={['Next.js', 'TypeScript', 'Supabase', 'Tailwind', 'Redux Toolkit', 'shadcn/ui']}
+                        badges={['Next.js', 'TypeScript', 'Supabase', 'Tailwind', 'Redux Toolkit', 'Cloudinary', 'shadcn/ui']}
                         liveUrl='https://pdd-app-xi.vercel.app/'
                         codeUrl='https://github.com/xtrphy/pdd-app'
-                    />
-
-                    <ProjectCard
-                        imageUrl='/extrahands-laptop.png'
-                        title='Extra Hands Team'
-                        description='Сайт посвященный игровой модификации для популярной визуальной новеллы "Зайчик", разрабатываемая командой Extra Hands.'
-                        badges={['Next.js', 'TypeScript', 'Tailwind', 'shadcn/ui']}
-                        liveUrl='https://extrahands-website.vercel.app/'
-                        codeUrl='https://github.com/xtrphy/extrahands-website'
                     />
 
                     <ProjectCard
@@ -40,9 +31,34 @@ const ProjectsSection = () => {
                         imageUrl='/mindmates-laptop.png'
                         title='MindMates Project'
                         description='Приложение для поддержания ментального здоровья. Делайте записи, управляйте своими мыслями и привычками.'
-                        badges={['Next.js', 'TypeScript', 'Prisma', 'Tailwind', 'shadcn/ui']}
+                        badges={['Next.js', 'TypeScript', 'Prisma', 'PostgreSQL', 'Tailwind', 'shadcn/ui']}
                         liveUrl='https://mind-mates-app.vercel.app/'
                         codeUrl='https://github.com/xtrphy/mind-mates-app'
+                    />
+
+                    <ProjectCard
+                        imageUrl='/extrahands-laptop.png'
+                        title='Extra Hands Team'
+                        description='Сайт посвященный игровой модификации для популярной визуальной новеллы "Зайчик", разрабатываемая командой Extra Hands.'
+                        badges={['Next.js', 'TypeScript', 'Tailwind', 'shadcn/ui']}
+                        liveUrl='https://extrahands-website.vercel.app/'
+                        codeUrl='https://github.com/xtrphy/extrahands-website'
+                    />
+
+                    <ProjectCard
+                        imageUrl='/vaultix-laptop.png'
+                        title='Google Drive Analog'
+                        description='Vaultix - аналог облачного хранилища от Google. Сохраняйте файлы, создавайте папки и делитесь ими с друзьями и коллегами.'
+                        badges={['EJS', 'Express', 'Prisma', 'PostgreSQL', 'Express-session', 'Passport', 'bcrypt', 'Cloudinary', 'shadcn/ui']}
+                        codeUrl='https://github.com/xtrphy/file-uploader'
+                    />
+
+                    <ProjectCard
+                        imageUrl='/blog-laptop.png'
+                        title='Xtrphy Blog'
+                        description='Личный блог с публикациями на тему веб-разработки. Админ-панель для управления контентом постов. Пишите комментарии и обсуждайте разные темы.'
+                        badges={['React', 'Express', 'REST API', 'Context API', 'CSS Modules']}
+                        codeUrl='https://github.com/xtrphy/blog-fullstack'
                     />
                 </div>
             </div>
---
+++ src/components/custom/ProjectCard.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import Image from 'next/image';
+import { Badge } from '../ui/badge';
+import { MoveUpRight } from 'lucide-react';
+import { FaGithub } from 'react-icons/fa';
+import ButtonWrapper from './ButtonWrapper';
+
+interface ProjectCardProps {
+    imageUrl: string;
+    title: string;
+    description: string;
+    badges: string[];
+    liveUrl?: string;
+    codeUrl?: string;
+};
+
+const ProjectCard = ({ imageUrl, title, description, badges, liveUrl, codeUrl }: ProjectCardProps) => {
+    return (
+        <div className='flex flex-col justify-start gap-3 w-[535px] rounded-2xl p-8 transition-transform duration-400 hover:scale-[1.015] bg-gradient-to-br from-[#a0a0a0]/90 to-[#0c0c0c]/50 backdrop-blur-xl border border-white/10 shadow-lg'>
+            <Image
+                src={imageUrl}
+                alt={title}
+                width={470}
+                height={470}
+                className='rounded-xl object-cover'
+            />
+
+            <h3 className='text-white text-2xl font-semibold mt-1'>{title}</h3>
+
+            <p className='text-[15px] text-[#e0e0e0] mb-3 tracking-wide leading-5'>{description}</p>
+
+            <div className='flex flex-wrap w-[420px] gap-2'>
+                {badges.map((badge, index) => (
+                    <Badge key={index} className='py-2 px-3 bg-[#333] backdrop-blur-md hover:bg-[#555] transition-colors duration-200'>
+                        {badge}
+                    </Badge>
+                ))}
+            </div>
+
+            <div className='ml-auto mt-auto flex gap-3'>
+                {liveUrl && (
+                    <ButtonWrapper content='Посмотреть'>
+                        <a
+                            href={liveUrl}
+                            target='_blank'
+                            className='flex items-center justify-center bg-white/70 w-10 h-10 p-2 rounded-md cursor-none hover:bg-gray-100 transition-colors duration-150'
+                        >
+                            <MoveUpRight />
+                        </a>
+                    </ButtonWrapper>
+                )}
+
+                {codeUrl && (
+                    <ButtonWrapper content='Код на Github'>
+                        <a
+                            href={codeUrl}
+                            target='_blank'
+                            className='flex items-center w-10 h-10 justify-center bg-white/70 p-2 rounded-md cursor-none hover:bg-gray-100 transition-colors duration-150'
+                        >
+                            <FaGithub size={30} />
+                        </a>
+                    </ButtonWrapper>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default ProjectCard;
