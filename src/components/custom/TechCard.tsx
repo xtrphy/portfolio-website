d30a87b2ca18dd403476935a5f1b0796883d8e12
xtrphy <perepeckinvlad@gmail.com>
2025-08-03 19:55:47 +0600

    styles: adapt stack section

--- src/components/custom/TechCard.tsx
+++ src/components/custom/TechCard.tsx
@@ -10,15 +10,15 @@ interface TechCardProps {
 
 const TechCard = ({ src, alt, title, bgColor }: TechCardProps) => {
     return (
-        <div className='bg-white/30 backdrop-blur-[5px] border border-white/40 rounded-2xl shadow-[0_4px_30px_rgba(0,0,0,0.1)] ring-1 ring-white/70 p-3 text-[#333] flex items-center gap-2 hover:scale-103 hover:ring-white hover:ring-2 hover:underline transition-all duration-200'>
+        <div className='w-full bg-white/30 backdrop-blur-[5px] border border-white/40 rounded-2xl shadow-[0_4px_30px_rgba(0,0,0,0.1)] ring-1 ring-white/70 p-3 text-[#333] flex items-center justify-start gap-2 hover:scale-103 hover:ring-white hover:ring-2 hover:underline transition-all duration-200'>
             <Image
                 src={src}
                 alt={alt}
-                width={48}
-                height={48}
-                className={`${bgColor} rounded-md p-1.5`}
+                width={40}
+                height={40}
+                className={`${bgColor} w-8 h-8 sm:w-10 sm:h-10 rounded-md p-1 sm:p-1.5`}
             />
-            <span className='text-[#121212] text-lg'>{title}</span>
+            <span className='text-[#121212] text-xs sm:text-lg font-medium'>{title}</span>
         </div>
     );
 };
